15c1f462c215a43731d8e386a5ae7b4f12a4353e
mohammed sarfaraz <sarfarazmohammed187@gmail.com>
2020-10-17 23:22:26 +0530

    working of last five profiles

--- app/app.js
+++ app/app.js
@@ -1,20 +1,57 @@
 $(document).ready(function () {
-    app.initialized()
-        .then(function (_client) {
-            var client = _client;
-            client.events.on('app.activated',
-                function () {
-                    client.data.get('loggedInAgent')
-                        .then(function (data) {
-                            $('#apptext').text("Agent logged in is " + data.loggedInAgent.email);
-                        })
-                        .catch(function (e) {
-                            client.interface.trigger("showNotify", {
-                                type: "danger",
-                                message: "Unable to retrieve logged-in agent details"
-                            });
-                            console.log('Exception - ', e);
-                        });
-                });
-        });
+  app.initialized().then(function (_client) {
+    window.client = _client;
+    client.events.on('app.activated', function () {
+      console.log("Event has triggered !!!");
+      $("#submit").on("fwClick", getPerson);
+    });
+  });
 });
+
+function getPerson() {
+  let fullName = $("#full_name").val(), email = $("#email").val(), phone_number = $("#phone_number").val();
+  fetchContactDetails(fullName, email, phone_number).then(function (payload) {
+    displayModal(payload);
+  }).catch(function (error) {
+    console.log(error);
+  });
+}
+
+function fetchContactDetails(fullName, email, phone_number) {
+  let opt = {
+    headers: {
+      "Authorization": "Bearer <%= iparam.apiKey %>",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      "full_name": fullName,
+      "email": email,
+      "phone": phone_number,
+    })
+  }
+  let url = "https://api.fullcontact.com/v3/person.enrich"
+  return new Promise(function (resolve) {
+    client.request.post(url, opt).then(function (data) {
+      resolve(JSON.parse(data.response));
+    }, function (err) {
+      let error = JSON.parse(err.response);
+      displayNotification("danger", error.message);
+    });
+  });
+}
+
+function displayModal(payload) {
+  client.interface.trigger("showModal", {
+    title: "Sample Modal",
+    template: "modal.html",
+    data: payload
+  }).then(function (data) {
+    console.log(data)
+  }).catch(function (error) {
+    console.log(error)
+  });
+}
+
+function displayNotification(type, message) {
+  client.interface.trigger('showNotify', { type: type, message: message });
+}
