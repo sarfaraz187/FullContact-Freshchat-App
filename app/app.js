11046f9daf6f6f92ed9968ce2137e84d3b3a7933
mohammed sarfaraz <sarfarazmohammed187@gmail.com>
2020-10-18 19:33:54 +0530

    Completed Basic features

--- app/app.js
+++ app/app.js
@@ -2,19 +2,93 @@ $(document).ready(function () {
   app.initialized().then(function (_client) {
     window.client = _client;
     client.events.on('app.activated', function () {
-      console.log("Event has triggered !!!");
+      $("#disp_profile").hide();
       $("#submit").on("fwClick", getPerson);
+      dispProfiles();
     });
   });
 });
+  
+let isDbPresent = false;
+let profileId = null;
 
 function getPerson() {
   let fullName = $("#full_name").val(), email = $("#email").val(), phone_number = $("#phone_number").val();
   fetchContactDetails(fullName, email, phone_number).then(function (payload) {
+    isDbPresent ? updateProfile(fullName, email, phone_number) : saveProfile(fullName, email, phone_number);
     displayModal(payload);
+    console.log({profileId});
+    profileId ? removeProfile(profileId) : "";
   }).catch(function (error) {
     console.log(error);
   });
+}
+
+function saveProfile(fullName, email, phone_number) {
+  let createObject = new Object();
+  let id = Math.floor(Math.random() * 1000000000);
+  createObject[`profile_${id}`] = { fullName, email, phone_number }
+  console.log({createObject});
+  client.db.set("profiles", createObject).then(function (data) {
+    console.log(data);
+    isDbPresent = true
+  }, function (error) {
+    console.log(error);
+  });
+}
+
+function updateProfile(fullName, email, phone_number) {
+  let updateObj = new Object();
+  let id = Math.floor(Math.random() * 1000000000);
+  updateObj[`profile_${id}`] = { fullName, email, phone_number }
+  console.log({updateObj});
+  client.db.update("profiles", "set", updateObj).then(function (data) {
+    console.log(data);
+  }, function (error) {
+    console.log(error);
+  });
+}
+
+function removeProfile (id) {
+  client.db.update("profiles","remove", [id]).then(function(data) {
+    console.log("!!!! Log from db Delete !!!",data);
+  }, function(error) {
+    console.log(error);
+  });
+}
+
+function dispProfiles() {
+  client.db.get("profiles").then(function (dbData) {
+    isDbPresent = true;
+    profileId = getLastId(dbData);
+    console.log(profileId);
+    let keysArr = Object.keys(dbData).reverse()
+    keysArr.forEach(element => {
+      let html = `<div class="lookup">
+        <label>Full Name</label>
+        <p>${dbData[element].fullName}</p>
+        <label>Email Address</label>
+        <p>${dbData[element].email}</p>
+        <label>Phone Number</label>
+        <p>${dbData[element].phone_number}</p>
+      </div>`
+    $("#disp_profile").append(html);
+    });
+    $("#disp_profile").show();
+  }, function (error) {
+    console.log("Error from Db : ", error);
+  });
+}
+
+function getLastId (dbData) {
+  let keys = Object.keys(dbData);
+  console.log(dbData[keys[0]]);
+  if(keys.length >= 5) {
+    return keys[0];
+  } else {
+    console.log("Number of profiles looked up are less than 5.");
+    return null
+  }
 }
 
 function fetchContactDetails(fullName, email, phone_number) {
@@ -35,6 +109,7 @@ function fetchContactDetails(fullName, email, phone_number) {
       resolve(JSON.parse(data.response));
     }, function (err) {
       let error = JSON.parse(err.response);
+      console.log(error)
       displayNotification("danger", error.message);
     });
   });
